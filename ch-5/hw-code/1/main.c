b1e87944bc78b1311413b5eaa456ce05f2408ea6
Mark Christopher C. Gemzon <markchristopher.gemzon@lexmark.com>
2022-04-26 17:58:59 +0800

    sync.

--- ch-5/hw-code/1/main.c
+++ ch-5/hw-code/1/main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-int x = 0;
-
 int main(int argc, char **argv) {
+    int x;
     printf("(pid %d)\t parent\n", (int)getpid());
     printf("(pid: %d)\t x = %d\n", (int)getpid(), x);
     x = 100;
     printf("(pid: %d)\t x = %d\n", (int)getpid(), x);
-    
     int rc = fork();
     
     if (rc < 0) {
@@ -20,9 +19,12 @@ int main(int argc, char **argv) {
         // child
         printf("(pid %d)\t child\n", (int)getpid());
         printf("(pid %d)\t x = %d\n", (int)getpid(), x);
+        x = x + 1;
+        printf("(pid %d)\t x = %d\n", (int)getpid(), x);
     } else {
         // parent
         wait(NULL);
+        x = x + 1;
         printf("(pid %d)\t parent\n", (int)getpid());
         printf("(pid %d)\t x = %d\n", (int)getpid(), x);
     }
